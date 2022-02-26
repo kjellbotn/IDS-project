bb320c7e1bdea955140cbd3bc17f0535a9724a4c
kjellbotn <natkjellbotn@gmail.com>
2022-02-25 17:54:45 -0800

    flashy flashy

--- testLED.py
+++ testLED.py
@@ -1,14 +1,19 @@
 import time
 import RPi.GPIO as GPIO
-LED = 5
+LED-r = 5
+LED-g = 22
 
 GPIO.setmode (GPIO.BCM)
-GPIO.setup (LED, GPIO.OUT)
+GPIO.setup (LED-r, GPIO.OUT)
+GPIO.setup (LED-g, GPIO.OUT)
+
 
 def flash (t):
-  GPIO.output(LED, True)
+  GPIO.output(LED-r, True)
+  GPIO.output (LED-g, False)
   time.sleep(1)
-  GPIO.output(LED, False)
+  GPIO.output(LED-r, False)
+  GPIO.output(LED-g, True)
   time.sleep(1)
 
 try:
