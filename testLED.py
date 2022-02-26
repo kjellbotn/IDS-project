6af1337fbecb2388c9444d1daf1c8244d667fa96
kjellbotn <natkjellbotn@gmail.com>
2022-02-25 18:22:28 -0800

    oh no

--- testLED.py
+++ testLED.py
@@ -2,11 +2,15 @@ import time
 import RPi.GPIO as GPIO
 LEDr = 5
 LEDg = 22
+BUTTON = 20
 
 GPIO.setmode (GPIO.BCM)
 GPIO.setup (LEDr, GPIO.OUT)
 GPIO.setup (LEDg, GPIO.OUT)
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON, GPIO.IN)
+ON = False
+display.setup(GPIO, LEDr, LEDg, ON)
 
 def flash (t):
   GPIO.output(LEDr, True)
@@ -27,7 +31,10 @@ def flash (t):
   GPIO.output(LEDr, False)
   GPIO.output(LEDg, True)
   time.sleep(1)
-  
+
+if BUTTON = ON:
+  GPIO.output (LEDg, False)
+  GPIO.output (LEDr, False)
 
 try:
   while True:
