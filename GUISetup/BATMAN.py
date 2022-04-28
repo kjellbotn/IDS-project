9f20c0fc6618b4c667fbc8df924434b1d2012401
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2022-04-27 22:38:43 -0700

    Added comments

--- GUISetup/BATMAN.py
+++ GUISetup/BATMAN.py
@@ -1,31 +1,45 @@
+#imports time, system, and pygame
 import time
 import sys
 import pygame
 
+#initiates pygame
 pygame.init()
 
+#sets up a display for pygame to use
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Caption?")
 
+#reads the batsignal image from file "BatSignal.png"
 BatSignal = pygame.image.load("BatSignal.png")
 
+#loops for as only as the program runs
 while True:
 
-    screen.fill((0, 0, 0))
-
-    screen.blit(BatSignal, (0, 0))
-
-
+    #loops for each pygame event
     for event in pygame.event.get():
+        #checks for quit event
         if(event.type == pygame.QUIT):
+            #stops the program
             pygame.quit()
             sys.exit()
 
+        #checks for keypressed event
         if(event.type == pygame.KEYDOWN):
+            #checks if the escape key was pressed
             if(event.key == pygame.K_ESCAPE):
+                #stops the program
                 pygame.quit()
                 sys.exit()
 
+    #makes the background black
+    screen.fill((0, 0, 0))
+
+    #draws the batsignal
+    screen.blit(BatSignal, (0, 0))
+
+    #updates the display
     pygame.display.update()
 
+    #waits to add delay for the next frame
     time.sleep(0.01)
