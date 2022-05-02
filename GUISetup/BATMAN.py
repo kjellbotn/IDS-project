ddc42262277c921f645885b535883c857ac3ba49
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2022-05-02 10:41:33 -0700

    added temp to Batman.py

--- GUISetup/BATMAN.py
+++ GUISetup/BATMAN.py
@@ -3,12 +3,57 @@ import time
 import sys
 import pygame
 
+#imports os and global for DS18B20
+import os
+import glob
+
 #imports board and the BME library
 import board
 from adafruit_bme280 import basic as BME280
 
 #initiates pygame
 pygame.init()
+
+#sets up thermometer as w1 device
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+#finds file for reading data from sensor
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+#read raw file data
+def readRaw():
+    #opens file
+    f = open(device_file, 'r')
+    #reads file
+    lines = f.readlines()
+    #closes file
+    f.close()
+
+    #returns read lines
+    return(lines)
+
+def readTemp():
+    #reads raw temp data
+    lines = readRaw()
+    #loops until reading works
+    while lines[0].strip()[-3:] != 'YES':
+        #waits and reads again
+        time.sleep(0.2)
+        lines = readRaw()
+    #finds the first occurence of t=
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        #reads the temperature
+        temp_string = lines[1][equals_pos+2:]
+        #converts to degrees C from string
+        temp_c = float(temp_string) / 1000.0
+        return(temp_c)
+
+    #returns nonsense on failiure
+    return(-100000)
 
 #sets up the I2C pins
 i2c = board.I2C()
@@ -111,6 +156,7 @@ while True:
         temp = "BME Temperature: " + str(bme280.temperature)
         press = "BME Pressure: " + str(bme280.pressure)
         humid = "BME Humidity: " + str(bme280.humidity)
+        DSTemp = "DS18B20 Temperature: " + str(readTemp())
 
         #writes the current temperature display
         DrawSurf.blit(font.render(temp, True, (255, 255, 255)), (50, 50))
@@ -120,6 +166,8 @@ while True:
 
         #writes the current humidity display
         DrawSurf.blit(font.render(humid, True, (255, 255, 255)), (50, 250))
+
+        DrawSurf.blit(font.render(DSTemp, True, (255, 255, 255)), (50, 350))
 
         #draws the buffered dispaly to the screen after resizing it
         screen.blit(pygame.transform.scale(DrawSurf, screenSize), (0, 0))
